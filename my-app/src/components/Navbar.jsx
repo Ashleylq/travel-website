3922b34253902379df06d29602a67d3607c97449
Ashleylq <ironmanashley@gmail.com>
2025-10-06 10:00:24 +0000

    create desktop background

---
+++ my-app/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+function Navbar(){
+    return(
+        <div>
+            <div className="logo">
+                <p>Bella Italia</p>
+                <p>Travel. Discover. Live.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
